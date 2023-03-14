f3af134baca44d892621ebbeab1539635f942c66
Yulya Yarkina <yulyayarkina@mbpsomthoryulya.home>
2023-03-14 18:03:16 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import sleep from "./assets/images/sleep.jpg";
+import "./App.css";
 
-function App() {
+const Logo = () => {
+  return <img src={logo} className="App-logo" alt="logo" />;
+};
+
+const Title = () => {
+  return (
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+  );
+};
+const Link = () => {
+  return (
+    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      Learn React
+    </a>
+  );
+};
+
+const Header = () => {
+  return <a href="/">Hi</a>;
+};
+
+const Img = () => {
+  return <img src={sleep} alt="sleep" />;
+};
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <Title />
+        <Link />
+        <Header />
+        <Img />
       </header>
     </div>
   );
 }
 
-export default App;
+//export default App;
